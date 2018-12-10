601ba13907a03d8a022cd4f5dbccc8ba31b63082
Sheldrick <mrmayrant@yahoo.com>
2018-12-10 09:27:22 -0500

    blog posts now receive information from redux state instead of local state

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,20 +1,9 @@
+
 const initState = {
     posts: [
-        {
-            id: 1,
-            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
-        },
-        {
-            id: 2,
-            title: "qui est esse",
-            body: "est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla"
-        },
-        {
-            id: 3,
-            title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-            body: "et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut"
-        }
+        { id: '1', title: 'Squirtle Laid an Egg', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat' },
+        { id: '2', title: 'Charmander Laid an Egg', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat' },
+        { id: '3', title: 'a Helix Fossil was Found', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat' }
     ]
 }
 
@@ -22,4 +11,4 @@ const rootReducer = (state = initState, action) => {
     return state;
 }
 
-export default rootReducer
+export default rootReducer;
